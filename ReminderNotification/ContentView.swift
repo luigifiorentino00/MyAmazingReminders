50dee0b89ef4e269ac0831a76cd01eb89fce6bb1
luigifiorentino00 <148210030+luigifiorentino00@users.noreply.github.com>
2023-11-14 12:17:24 +0100

    Commit
    fixed project folder

---
+++ ReminderNotification/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReminderNotification
+//
+//  Created by Luigi Fiorentino on 14/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("ciaone mi sparo ora")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReminderNotification/ReminderNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderNotificationApp.swift
+//  ReminderNotification
+//
+//  Created by Luigi Fiorentino on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ReminderNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
