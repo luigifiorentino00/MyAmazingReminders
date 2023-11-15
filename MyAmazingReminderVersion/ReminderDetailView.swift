d65eee9e3ce1033d1551463f0d701c4990a1cdb2
luigifiorentino00 <148210030+luigifiorentino00@users.noreply.github.com>
2023-11-15 19:14:55 +0100

    Info button, note (not working properly), weekday and time of the reminder

---
+++ MyAmazingReminderVersion/ReminderDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ReminderDetailView.swift
+//  MyAmazingReminderVersion
+//
+//  Created by Luigi Fiorentino on 15/11/23.
+//
+
+import SwiftUI
+
+struct ReminderDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReminderDetailView()
+}
