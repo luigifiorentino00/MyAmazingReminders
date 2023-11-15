d308a4c5a1a700d12216383178a9b9266674b293
luigifiorentino00 <148210030+luigifiorentino00@users.noreply.github.com>
2023-11-15 12:55:48 +0100

    Title and first notification implementation

---
+++ MyAmazingReminderVersion/LocalNotification.swift
@@ -0,0 +1,8 @@
+//
+//  LocalNotification.swift
+//  MyAmazingReminderVersion
+//
+//  Created by Luigi Fiorentino on 15/11/23.
+//
+
+import Foundation
---
+++ MyAmazingReminderVersion/ReminderView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  MyAmazingReminderVersion
+//
+//  Created by Luigi Fiorentino on 15/11/23.
+//
+
+import SwiftUI
+
+struct ReminderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReminderView()
+}
