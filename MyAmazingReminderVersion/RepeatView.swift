63f02a260596b249ad980c833b8b3d58f09e98bb
luigifiorentino00 <148210030+luigifiorentino00@users.noreply.github.com>
2023-11-21 13:01:49 +0100

    accessibility features, repeat picker (does not wok)

---
+++ MyAmazingReminderVersion/RepeatView.swift
@@ -0,0 +1,18 @@
+//
+//  RepeatView.swift
+//  MyAmazingReminderVersion
+//
+//  Created by Luigi Fiorentino on 20/11/23.
+//
+
+import SwiftUI
+
+struct RepeatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RepeatView()
+}
